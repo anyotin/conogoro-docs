8dcbbeede61664c1d0a076f391cd146c1093a31d
y.tanino <y.tanino@advancednet.co.jp>
2023-07-04 01:33:30 +0900

    [テーブル定義]M_閲覧

--- conogoro.sql
+++ conogoro.sql
@@ -2,12 +2,24 @@ SET SESSION FOREIGN_KEY_CHECKS=0;
 
 /* Drop Tables */
 
+DROP TABLE IF EXISTS m_browsing;
 DROP TABLE IF EXISTS m_short;
 
 
 
 
 /* Create Tables */
+
+CREATE TABLE m_browsing
+(
+	id smallint NOT NULL,
+	page_name varchar(30) NOT NULL,
+	browsing_count int NOT NULL,
+	in_date datetime NOT NULL,
+	up_date datetime NOT NULL,
+	PRIMARY KEY (id)
+);
+
 
 CREATE TABLE m_short
 (
