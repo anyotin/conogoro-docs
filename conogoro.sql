d5dfa4c9d4123e5b3c84c1701ad3d768243a35af
y.tanino <y.tanino@advancednet.co.jp>
2023-07-05 02:32:27 +0900

    [テーブル定義]M_閲覧修正

--- conogoro.sql
+++ conogoro.sql
@@ -2,24 +2,13 @@ SET SESSION FOREIGN_KEY_CHECKS=0;
 
 /* Drop Tables */
 
-DROP TABLE IF EXISTS m_browsing;
 DROP TABLE IF EXISTS m_short;
+DROP TABLE IF EXISTS s_browsing;
 
 
 
 
 /* Create Tables */
-
-CREATE TABLE m_browsing
-(
-	id smallint NOT NULL,
-	page_name varchar(30) NOT NULL,
-	browsing_count int NOT NULL,
-	in_date datetime NOT NULL,
-	up_date datetime NOT NULL,
-	PRIMARY KEY (id)
-);
-
 
 CREATE TABLE m_short
 (
@@ -31,6 +20,27 @@ CREATE TABLE m_short
 	in_date datetime NOT NULL,
 	up_date datetime NOT NULL,
 	PRIMARY KEY (id)
+);
+
+
+CREATE TABLE s_browsing
+(
+	id smallint NOT NULL,
+	-- 1 : ショートショート
+	-- 2 : 創作料理
+	-- 3 : 文具紹介
+	-- 4 : マッチングアプリ冒険譚
+	-- 5 : ゲーム紹介
+	topic_type tinyint NOT NULL COMMENT '1 : ショートショート
+2 : 創作料理
+3 : 文具紹介
+4 : マッチングアプリ冒険譚
+5 : ゲーム紹介',
+	page_no int NOT NULL,
+	browsing_count int NOT NULL,
+	in_date datetime NOT NULL,
+	up_date datetime NOT NULL,
+	PRIMARY KEY (id, topic_type, page_no)
 );
 
 
